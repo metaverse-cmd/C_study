769d7c922891a457498e6ae30ed741e96f56c93a
metaverse-cmd <shengmingqiucheng@gmail.com>
2025-10-01 16:46:16 +0800

    Add files via upload

---
+++ class_1001/class_1001/class_1001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+	_Bool flag1 = false;
+	_Bool flag2 = true;
+	if (flag1)
+		printf("hehe");
+	if (flag2)
+		printf("haha");
+	//printf("abcnasd\n");
+	//printf("abc\nasd");
+	//printf("%c", '\'');
+	//printf("%s", "\""); 
+	//printf("abcdef");
+	//printf("\b");
+	//printf("xxx");
+	return 0;  
+}
